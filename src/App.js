312d6bc9d2a7394ce08022f0759c57c64366a75c
cgynlai <cgynlai@gmail.com>
2022-03-24 16:44:05 +0800

    created redux errorsReducer and map states to component props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
