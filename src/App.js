3f77b174b26bb41e093b49ed6173f91d2f3224c9
cgynlai <cgynlai@gmail.com>
2022-04-26 23:12:38 +0800

    constructed state-props-formInputfield

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 //import ProjectBoard from "./components/ProjectBoard";
 
 function App() {
@@ -25,6 +26,10 @@ function App() {
           <Route path="/updateProject/:id" component={UpdateProject} />
           <Route path="/projectBoard/:id" component={ProjectBoard} />
           <Route path="/addProjectTask/:id" component={AddProjectTask} />
+          <Route
+            path="/UpdateProjectTask/:backlog_id/:pt_id"
+            component={UpdateProjectTask}
+          />
         </div>
       </Router>
     </Provider>
